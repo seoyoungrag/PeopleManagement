bafca67ba5f354b22b462c9cee88f350aa508d92
yr.seo <yr.seo@dwebss.co.kr>
2019-07-23 22:55:12 +0900

    yellowbox ignore추가

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import './app/config'
 import { registerScreensAndStartApp } from './app/navigation/layouts'
+import {YellowBox} from 'react-native';
+
+YellowBox.ignoreWarnings([
+    'Remote debugger is in a background tab which may',
+    'Require cycle:',
+    'Unable to define method \'getConstants()\' on'
+    ]);
 
 // this registers the screens and starts the react-native-navigation process
 registerScreensAndStartApp()
